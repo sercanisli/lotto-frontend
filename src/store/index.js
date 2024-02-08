dfdab58c9f14134e1b7032a79e8a8ac4c1d3d7c3
Sercan ISLI <sercanislii@gmail.com>
2024-02-09 02:57:40 +0300

    index.js created in store folder for configure store.

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { superLotoApi } from './apis/superLotoApi';
+
+export const store = configureStore({
+    reducer: {
+        [superLotoApi.reducerPath]: superLotoApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+            .concat(superLotoApi.middleware);
+    },
+});
+
+setupListeners(store.dispatch);
+
+export { useFetchSuperLotoQuery, useAddSuperLotoMutation, useRemoveSuperLotoMutation } from './apis/superLotoApi';
