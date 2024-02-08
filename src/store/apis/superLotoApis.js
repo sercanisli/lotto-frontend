7ad85380db3a549d3d77ad5b841725a13d05eb04
Sercan ISLI <sercanislii@gmail.com>
2024-02-09 02:51:41 +0300

    superLotoApis created.

---
+++ src/store/apis/superLotoApis.js
@@ -0,0 +1,60 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const superLotoApi = createApi({
+    reducerPath:'superLoto',
+    baseQuery: fetchBaseQuery({
+        baseUrl:'https://localhost:7135/',
+        fetchFn: (...args) => {
+            return fetch(...args);
+        },
+    }),
+    endpoints(builder) {
+        return {
+            fetchSuperLoto:builder.query({
+                providesTags:['SuperLoto'],
+                query: () => {
+                    return {
+                        url :'/api/superloto?pageSize=10&pageNumber=1',
+                        method:'GET',
+                    };
+                },
+            }),
+            addSuperLoto:builder.mutation({
+                invalidatesTags: () => {
+                    return [{type: 'SuperLoto'}];
+                },
+                query : () => {
+                    return {
+                        url:'/api/superloto',
+                        method:'POST',
+                        body: {
+                            numbers:[
+                                5,
+                                10,
+                                15,
+                                20,
+                                25,
+                                30
+                            ],
+                            Date: "30.12.2023 00:00:00"
+                        },
+                    };
+                },
+            }),
+            removeSuperLoto: builder.mutation({
+                invalidatesTags:() => {
+                    return[{type:'SuperLoto'}]
+                },
+                query:(superloto) => {
+                    return {
+                        url:`/api/sayisalloto/${superloto.id}`,
+                        method:'DELETE'
+                    };
+                },
+            }),
+        };
+    },
+});
+
+export const { useFetchSuperLotoQuery, useAddSuperLotoMutation, useRemoveSuperLotoMutation } = superLotoApi;
+export {superLotoApi};
