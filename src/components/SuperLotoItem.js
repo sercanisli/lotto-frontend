e53a7d4b78684960f82f6242e859173af41ca8ff
Sercan ISLI <sercanislii@gmail.com>
2024-02-12 02:51:39 +0300

    Successfully printed data received as property in SuperLotoItem.

--- src/components/SuperLotoItem.js
+++ src/components/SuperLotoItem.js
@@ -3,17 +3,22 @@ import { Stack, Skeleton, Box, Card, CardActions, CardContent, CardMedia, Typogr
 import image from '../assets/superlotoLogo.jpg';
 import '../styles/superLotoItem.css';
 
-function SuperLotoItem() {
+function SuperLotoItem({superLoto}) {
   return (
     <Card className='card'>
       <CardMedia className='image' component="img" image={image} />
       <CardContent>
         <Typography>
-          Tarih
+          {superLoto.Date}
         </Typography>
        <Typography >
-        <Stack direction="row" >
-          Rakamlar
+        <Stack direction="row" className='numbers'>
+          <p>{superLoto.Numbers[0]}</p>
+          <p>{superLoto.Numbers[1]}</p>
+          <p>{superLoto.Numbers[2]}</p>
+          <p>{superLoto.Numbers[3]}</p>
+          <p>{superLoto.Numbers[4]}</p>
+          <p>{superLoto.Numbers[5]}</p>
         </Stack>
        </Typography>
       </CardContent>
