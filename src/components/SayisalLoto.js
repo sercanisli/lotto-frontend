e207e814fc3b3854456f23a9e0316e39b64bdaac
Sercan ISLI <sercanislii@gmail.com>
2024-02-19 16:00:37 +0300

    Headers import with axios library for SayisalLoto Component completed

--- src/components/SayisalLoto.js
+++ src/components/SayisalLoto.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { useFetchSayisalLotoQuery } from '../store/apis/sayisalLotoApi';
+import '../styles/sayisalLoto.css';
+import Pagination, { paginationClasses } from '@mui/material/Pagination';
+import axios from 'axios';
+import { Typography, CircularProgress} from '@mui/material';
+
 
 function SayisalLoto() {
+
+  const [lastOne, setLastOne] = useState(null);
+  const [totalPage, setTotalPage] = useState(null);
+  const [selectedPage, setSelectedPage] = useState(1);
+
+  debugger;
+
+  useEffect(() => {
+    axios.get(`https://localhost:7135/api/sayisalloto?pageSize=${page.pageSize}&pageNumber=${page.pageNumber}`, {
+        headers: {
+            'Accept':'application/json'
+        }
+    })
+      .then(response => {
+        const lastOneData = response.data[0];
+        setLastOne(lastOneData);
+        const xPaginationHeader = response.headers['x-pagination'];
+        const xPaginationData = JSON.parse(xPaginationHeader);
+        const totalPage = xPaginationData.TotalPage;
+        setTotalPage(totalPage);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const page = {
+    pageSize:10,
+    pageNumber:selectedPage
+  }
+
+  console.log(totalPage);
+
   return (
-    <div>SayisalLotooo</div>
+    <div>
+
+    </div>
   )
 }
 
