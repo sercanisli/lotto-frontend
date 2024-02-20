53759397bbbe141c0ebb1904599aa5bb80796f5a
Sercan ISLI <sercanislii@gmail.com>
2024-02-21 02:01:50 +0300

    Data for the OnNumara Component was obtained using Redux Toolkit and metadata was obtained using axios.

--- src/components/OnNumara.js
+++ src/components/OnNumara.js
@@ -1,6 +1,69 @@
 import React, { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { useFetchOnNumaraQuery } from '../store/apis/onNumaraApi';
+import '../styles/sayisalLoto.css';
+import Pagination, { paginationClasses } from '@mui/material/Pagination';
+import axios from 'axios';
+import { Typography, CircularProgress} from '@mui/material';
 
 function OnNumara() {
+
+  const [lastOne, setLastOne] = useState(null);
+  const [totalPage, setTotalPage] = useState(null);
+  const [selectedPage, setSelectedPage] = useState(1);
+
+  const handlePageChange = (event, page) => {
+    setSelectedPage(page);
+  };
+
+  useEffect(() => {
+    axios.get(`https://localhost:7135/api/onnumara?pageSize=${page.pageSize}&pageNumber=${page.pageNumber}`, {
+        headers: {
+            'Accept':'application/json'
+        }
+    })
+      .then(response => {
+        const lastOneData = response.data[0];
+        setLastOne(lastOneData);
+        const xPaginationHeader = response.headers['x-pagination'];
+        const xPaginationData = JSON.parse(xPaginationHeader);
+        const totalPage = xPaginationData.TotalPage;
+        setTotalPage(totalPage);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const page = {
+    pageSize:10,
+    pageNumber:selectedPage
+  }
+
+  console.log(totalPage);
+
+
+  const {data, isError, isFetching} = useFetchOnNumaraQuery(page);
+        if(isFetching || data  === undefined){
+            return (
+              <CircularProgress className='spinner' />
+            )
+        }
+    console.log(data);
+    
+
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      }));
+
+
   return (
     <div>OnNumaraaa</div>
   )
