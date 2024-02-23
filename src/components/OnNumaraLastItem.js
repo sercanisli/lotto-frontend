cf1cc7417a8660ded57c8fa163a3fbb300ffc1f0
Sercan ISLI <sercanislii@gmail.com>
2024-02-23 17:11:14 +0300

    onNumaraLastItemand component and onNumaraLastItem.css created and applied.

--- src/components/OnNumaraLastItem.js
+++ src/components/OnNumaraLastItem.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { CircularProgress, Stack, Typography } from '@mui/material';
+import React from 'react';
+import '../styles/onNumaraLastItem.css';
+import {formatDate} from './dateUtils';
 
-function OnNumaraLastItem() {
-  return (
-    <div>OnNumaraLastItem</div>
-  )
+function OnNumaraLastItem({lastOnNumara}) {
+    if (!lastOnNumara) {
+      return (
+          <p><CircularProgress /></p>
+      );
+  }
+    const date = formatDate(lastOnNumara.Date);
+    return (
+      <>
+          <Typography>
+            <Stack direction="row" className='numbersOnNumaraLastItem'>
+              {lastOnNumara.Numbers.slice(0, 11).map((number, index) => (
+                <p key={index}>{number}</p>
+              ))}
+            </Stack>
+            <Stack direction="row" className='numbersOnNumaraLastItem'>
+              {lastOnNumara.Numbers.slice(11).map((number, index) => (
+                <p key={index + 11}>{number}</p>
+              ))}
+            </Stack>
+          </Typography>
+          {
+          lastOnNumara.Date ? (
+            <p className='dateOnNumaraLastItem'> Tarih : {date}</p>
+            ) : (
+              <p><CircularProgress/></p>
+            )
+          }
+      </>
+      
+    )
 }
 
 export default OnNumaraLastItem
