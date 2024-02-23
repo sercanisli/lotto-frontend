a6d641ff06630fed031a86fe2dcc2450e123d6f4
Sercan ISLI <sercanislii@gmail.com>
2024-02-23 12:33:39 +0300

    onNumaraItem.css created and applied in OnNumaraItem component.

--- src/components/OnNumaraItem.js
+++ src/components/OnNumaraItem.js
@@ -2,26 +2,27 @@ import React from 'react';
 import { Stack, Skeleton, Box, Card, CardActions, CardContent, CardMedia, Typography, Grid } from '@mui/material';
 import image from '../assets/onNumaraLogo.png';
 import {formatDate} from './dateUtils';
+import '../styles/onNumaraItem.css';
 
 
 function OnNumaraItem({onNumara}) {
   return (
-    <Grid container xs={12} md={12} direction="row"  alignItems="center" className='grid'>
-      <Card className='card'>
+    <Grid container xs={12} md={12} direction="row"  alignItems="center" className='gridOnNumaraItem'>
+      <Card className='cardOnNumaraItem'>
       <CardContent>
-        <Stack direction="row" className='imageAndDate'>
-          <CardMedia className='image' component="img" image={image} />
-          <Typography className='date'>
+        <Stack direction="row" className='imageAndDateOnNumaraItem'>
+          <CardMedia className='imageOnNumaraItem' component="img" image={image} />
+          <Typography className='dateOnNumaraItem'>
             Tarih : {formatDate(onNumara.Date)}
           </Typography>
         </Stack>
         <Typography >
-          <Stack direction="row" className='numbers'>
+          <Stack direction="row" className='numbersOnNumaraItem'>
             {onNumara.Numbers.slice(0, 11).map((number, index) => (
               <p key={index}>{number}</p>
             ))}
           </Stack>
-          <Stack direction="row" className='numbers'>
+          <Stack direction="row" className='numbersOnNumaraItem'>
             {onNumara.Numbers.slice(11).map((number, index) => (
               <p key={index + 11}>{number}</p>
             ))}
