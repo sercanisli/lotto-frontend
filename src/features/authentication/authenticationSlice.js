3c09cd4ae503f244b07342efdd2a5c5a6361b9c5
Sercan ISLI <sercanislii@gmail.com>
2024-02-26 15:53:48 +0300

    autheticationSlice created.

---
+++ src/features/authentication/authenticationSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authenticationSlice = createSlice({
+    name : 'authentication',
+    initialState : {user : null, token : null},
+    reducers: {
+        setCredentials: (state, action) => {
+            const {user, accessToken} = action.payload
+            state.user = user,
+            state.token = accessToken
+        },
+        logOut: (state, action) => {
+            state.user = null
+            state.token = null
+        }
+    },
+})
+
+export const {setCredentials, logOut} = authenticationSlice.actions;
+
+export default authenticationSlice.reducer;
+
+export const selectCurrentUser = (state) => state.authentication.user
+export const selectCurrentToken = (state) => state.authentication.token
--- src/store/apis/authenticationApi.js
+++ src/store/apis/authenticationApi.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { setCredentials, logOut } from '../../features/authentication/authenticationSlice'; 
 
 const authenticationApi = createApi ({
     reducerPath:'authenctication',
