93c7a796552f826d6e23c555e56f97f6c520f98d
Sercan ISLI <sercanislii@gmail.com>
2024-02-26 03:13:54 +0300

    Refactoring in Login component.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/login.css';
 import { useLoginUserMutation } from '../store/apis/authenticationApi';
 import { useState } from 'react';
+import {toast} from 'react-toastify'
+import { useNavigate } from 'react-router-dom'; 
 
 
 function Login() {
@@ -9,43 +11,40 @@ function Login() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
+  const navigate = useNavigate();
 
-  const handleClick = () => {
-    const user = {
-      username:name, 
-      password:password
+  const [loginUser, {data, isSuccess, isError, error }] = useLoginUserMutation();
+
+  const user = {
+    username:name,
+    password:password
+  }
+  const handleLogin = async () => {
+    if(user) {
+      await loginUser({user});
     }
-    const {data, isError, isFetching} = useLoginUserMutation(user);
-  } 
-
+    else {
+      toast.error("Boş yer kalmamalı");
+    }
+  };
   
+  useEffect(() => {
+    if(isSuccess) {
+      toast.success("Successfly");
+      navigate("/");
+    }
+  })
+
   console.log(data);
 
-  if(!data)
-  {
-    return (
-      <div className="login-container">
-        <form className="login-form" action="#" method="post">
-          <h2>Giriş Yap</h2>
-          <input type="text" name="username" placeholder="Kullanıcı Adı" required value={name} onChange={(e) => setName(e.target.value)}/>
-          <input type="password" name="password" placeholder="Şifre" required value={password} onChange={(e) => setPassword(e.target.value)}/>
-          <div className='submitGroups'>
-            <button type="submit" onClick={handleClick}>Giriş Yap</button>
-            <p>Hesabınız yok mu? <a href="/signin">Hesap Oluştur</a></p>
-          </div>
-        </form>
-      </div>
-    )
-  }
-  
   return (
     <div className="login-container">
-      <form className="login-form" action="#" method="post">
+      <form className="login-form" method="post">
         <h2>Giriş Yap</h2>
         <input type="text" name="username" placeholder="Kullanıcı Adı" required value={name} onChange={(e) => setName(e.target.value)}/>
         <input type="password" name="password" placeholder="Şifre" required value={password} onChange={(e) => setPassword(e.target.value)}/>
         <div className='submitGroups'>
-          <button type="submit" onClick={handleClick}>Giriş Yap</button>
+          <button type="submit" onClick={handleLogin}>Giriş Yap</button>
           <p>Hesabınız yok mu? <a href="/signin">Hesap Oluştur</a></p>
         </div>
       </form>
