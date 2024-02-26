89599192ff4f6ec50e1fabf45e2afa687852f2f4
Sercan ISLI <sercanislii@gmail.com>
2024-02-27 02:07:01 +0300

    reducer and middleware definitions were made for sansTopuApi.

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import { superLotoApi } from './apis/superLotoApi';
 import { authenticationApi } from "./apis/authenticationApi";
 import { sayisalLotoApi } from './apis/sayisalLotoApi';
 import { onNumaraApi } from "./apis/onNumaraApi";
+import { sansTopuApi } from './apis/sansTopuApi';
 
 export const store = configureStore({
     reducer: {
@@ -11,13 +12,15 @@ export const store = configureStore({
         [superLotoApi.reducerPath]: superLotoApi.reducer,
         [sayisalLotoApi.reducerPath]: sayisalLotoApi.reducer,
         [onNumaraApi.reducerPath]: onNumaraApi.reducer,
+        [sansTopuApi.reducerPath]: sansTopuApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
             .concat(authenticationApi.middleware)
             .concat(superLotoApi.middleware)
             .concat(sayisalLotoApi.middleware)
-            .concat(onNumaraApi.middleware);
+            .concat(onNumaraApi.middleware)
+            .concat(sansTopuApi.middleware);
     },
 });
 
@@ -27,3 +30,4 @@ export { useLoginUserMutation} from './apis/authenticationApi';
 export { useFetchSuperLotoQuery, useAddSuperLotoMutation, useRemoveSuperLotoMutation, useGetRandomNumbersForSuperLotoQuery } from './apis/superLotoApi';
 export { useFetchSayisalLotoQuery, useGetRandomNumbersForSayisalLotoQuery } from './apis/sayisalLotoApi';
 export { useFetchOnNumaraQuery, useGetRandomNumbersForOnNumaraQuery } from './apis/onNumaraApi';
+export { useFetchSansTopuQuery, useGetRandomNumbersForSansTopuQuery } from './apis/sansTopuApi';
