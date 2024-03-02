412adfcc9855a8b8a2eacf2db449dff1b85c5261
Sercan ISLI <sercanislii@gmail.com>
2024-03-02 03:14:42 +0300

    Refactoring in index.js which store.

---
+++ src/features/authentication/authenticationApiSlice.js
@@ -0,0 +1,14 @@
+import { authenticationApi } from "../../store/apis/authenticationApi";
+
+export const authenticationApiSlice = authenticationApi.injectEndpoints({
+    endpoints:builder => ({
+        login : builder.mutation({
+            query : credentials => ({
+                url: 'api/authentication/login',
+                method : 'POST',
+                body : {...credentials}
+            })
+        }),
+        
+    })
+})
--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { superLotoApi } from './apis/superLotoApi';
-import { authenticationApi } from "./apis/authenticationApi";
 import { sayisalLotoApi } from './apis/sayisalLotoApi';
 import { onNumaraApi } from "./apis/onNumaraApi";
 import { sansTopuApi } from './apis/sansTopuApi';
+import { authenticationApi } from "./apis/authenticationApi";
+import authReducer from '../features/authentication/authenticationSlice'
 
 export const store = configureStore({
     reducer: {
-        [authenticationApi.reducerPath] : authenticationApi.reducer,
         [superLotoApi.reducerPath]: superLotoApi.reducer,
         [sayisalLotoApi.reducerPath]: sayisalLotoApi.reducer,
         [onNumaraApi.reducerPath]: onNumaraApi.reducer,
         [sansTopuApi.reducerPath]: sansTopuApi.reducer,
+        [authenticationApi.reducerPath] : authenticationApi.reducer,
+        auth : authReducer
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
-            .concat(authenticationApi.middleware)
             .concat(superLotoApi.middleware)
             .concat(sayisalLotoApi.middleware)
             .concat(onNumaraApi.middleware)
-            .concat(sansTopuApi.middleware);
+            .concat(sansTopuApi.middleware)
+            .concat(authenticationApi.middleware);
     },
+    devTools:true
 });
 
 setupListeners(store.dispatch);
 
-export { useLoginUserMutation} from './apis/authenticationApi';
 export { useFetchSuperLotoQuery, useAddSuperLotoMutation, useRemoveSuperLotoMutation, useGetRandomNumbersForSuperLotoQuery } from './apis/superLotoApi';
 export { useFetchSayisalLotoQuery, useGetRandomNumbersForSayisalLotoQuery } from './apis/sayisalLotoApi';
 export { useFetchOnNumaraQuery, useGetRandomNumbersForOnNumaraQuery } from './apis/onNumaraApi';
