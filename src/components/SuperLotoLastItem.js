8d4c71f7e64031d9252518b3330ca8472a6a1fa3
Sercan ISLI <sercanislii@gmail.com>
2024-03-11 02:34:18 +0300

    LastItem in SuperLotoLastItem component started to be received with api request.

--- src/components/SuperLotoLastItem.js
+++ src/components/SuperLotoLastItem.js
@@ -2,27 +2,32 @@ import { CircularProgress, Stack, Typography } from '@mui/material';
 import React from 'react';
 import '../styles/superLotoLastItem.css';
 import {formatDate} from './dateUtils';
+import { useGetSuperLotoLastItemQuery } from '../store/apis/superLotoApi';
 
-function SuperLotoLastItem({lastSuperLoto}) {
-  if (!lastSuperLoto) {
+function SuperLotoLastItem() {
+
+  const {data, isError, isFetching} = useGetSuperLotoLastItemQuery();
+  console.log(data);
+
+  if (!data) {
     return (
         <p><CircularProgress /></p>
     );
 }
   
-  const date = formatDate(lastSuperLoto.Date);
+  const date = formatDate(data.date);
   return (
     <>
         <Stack direction="row" className='numbersSuperLotoLastItem'>
-            <p>{lastSuperLoto.Numbers[0]}</p>
-            <p>{lastSuperLoto.Numbers[1]}</p>
-            <p>{lastSuperLoto.Numbers[2]}</p>
-            <p>{lastSuperLoto.Numbers[3]}</p>
-            <p>{lastSuperLoto.Numbers[4]}</p>
-            <p>{lastSuperLoto.Numbers[5]}</p>
+            <p>{data.numbers[0]}</p>
+            <p>{data.numbers[1]}</p>
+            <p>{data.numbers[2]}</p>
+            <p>{data.numbers[3]}</p>
+            <p>{data.numbers[4]}</p>
+            <p>{data.numbers[5]}</p>
         </Stack>
         {
-        lastSuperLoto.Date ? (
+        data.date ? (
           <p className='dateSuperLotoLastItem'> Tarih : {date}</p>
           ) : (
             <p><CircularProgress/></p>
