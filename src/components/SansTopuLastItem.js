754eda7130212e82f3ac826a589a26ad2a367cf5
Sercan ISLI <sercanislii@gmail.com>
2024-03-15 01:50:45 +0300

    getSansTopuLastItem api request used in SansTopuLastItem component.

--- src/components/SansTopuLastItem.js
+++ src/components/SansTopuLastItem.js
@@ -2,35 +2,38 @@ import { CircularProgress, Stack, Typography } from '@mui/material';
 import React from 'react';
 import {formatDate} from './dateUtils';
 import '../styles/sansTopuLastItem.css'
+import { useGetSansTopuLastItemQuery } from '../store/apis/sansTopuApi';
 
-function SansTopuLastItem({lastSansTopu}) {
-
-  if (!lastSansTopu) {
+function SansTopuLastItem() {
+  const {data, isError, isFetching} = useGetSansTopuLastItemQuery();
+  console.log(data);
+ 
+  if (!data) {
     return (
         <p><CircularProgress /></p>
     );
-}
-
-  const date = formatDate(lastSansTopu.Date);
+  }
+  
+  const date = formatDate(data.date);
   
   return (
     <>
     <Stack className='sansTopuLastItem' direction="row">
       <Stack direction="row" className='numbersSansTopuLastItem'>
-            <p>{lastSansTopu.Numbers[0]}</p>
-            <p>{lastSansTopu.Numbers[1]}</p>
-            <p>{lastSansTopu.Numbers[2]}</p>
-            <p>{lastSansTopu.Numbers[3]}</p>
-            <p>{lastSansTopu.Numbers[4]}</p>
+            <p>{data.numbers[0]}</p>
+            <p>{data.numbers[1]}</p>
+            <p>{data.numbers[2]}</p>
+            <p>{data.numbers[3]}</p>
+            <p>{data.numbers[4]}</p>
       </Stack>
       <Stack direction="row">
         <p className='plusSansTopuLastItem'>+</p>
-        <p className='plusNumberSansTopuLastItem'>{lastSansTopu.PlusNumber}</p>
+        <p className='plusNumberSansTopuLastItem'>{data.plusNumber}</p>
       </Stack>
     </Stack>
       
         {
-        lastSansTopu.Date ? (
+        data.date ? (
           <p className='dateSansTopuLastItem'> Tarih : {date}</p>
           ) : (
             <p><CircularProgress/></p>
