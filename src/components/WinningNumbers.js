af83591cc03e53e6caa4913381743dcb47753d6c
Sercan ISLI <sercanislii@gmail.com>
2024-03-30 03:59:35 +0300

    Successfully defined response in WinningNumbers component.

--- src/components/WinningNumbers.js
+++ src/components/WinningNumbers.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { useFetchWinningNumbersQuery} from '../store/apis/winningNumbersApi';
+import { Typography, CircularProgress} from '@mui/material';
 
 const WinningNumbers = () => {
     const [id, setId] = useState(1)
     const {data, isError, isFetching} = useFetchWinningNumbersQuery(id);
+    if(isFetching || data  === undefined){
+        return (
+          <CircularProgress className='spinnerWinningNumbers' />
+        )
+    }
     console.log(data);
   return (
     <div>WinningNumbers</div>
