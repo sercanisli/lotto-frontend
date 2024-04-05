5dd5cea3279036ab4c21cfd210fd1e2784539906
Sercan ISLI <sercanislii@gmail.com>
2024-04-06 01:30:49 +0300

    Footer was done.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/footer.css';
 
 function Footer() {
   return (
     <div className='footer'>
-        <div>
-            <p className='footerDescription'>
-                Tüm hakları saklıdır | Lotocum
-            </p>
-        </div>
+      <div className='footerLinks'>
+        <Link to="/">
+          Ana Sayfa
+        </Link>
+        <Link to="/sayisalloto">
+          Sayısal Loto
+        </Link>
+        <Link to="/superloto">
+          Super Loto
+        </Link>
+        <Link to="/onnumara">
+          On Numara
+        </Link>
+        <Link to="/sanstopu">
+          Şans Topu
+        </Link>
+        <Link to="/iletisim">
+          İletişim
+        </Link>
+        <Link to="/hakkimizda">
+          Hakkımızda
+        </Link>
+      </div>
+      <div className='footerDescription'>
+        <p>
+          Tüm hakları saklıdır | Lotocum
+        </p>
+      </div>
     </div>
   )
 }
