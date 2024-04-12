fb731df3ef6fa6568e9365074a84b024ab851f92
Sercan ISLI <sercanislii@gmail.com>
2024-04-13 01:46:44 +0300

    GetAll request for SayisalLoto was implemented in AdminSayisalLoto Component.

--- src/components/AdminSayisalLoto.js
+++ src/components/AdminSayisalLoto.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useAxiosForSayisalLotoPagination } from './axiosComponents/axiosForSayisalLotoPagination';
+import { useFetchSayisalLotoQuery } from '../store/apis/sayisalLotoApi';
+import Box from '@mui/material/Box';
+import { Typography, CircularProgress} from '@mui/material';
+
 
 const AdminSayisalLoto = () => {
+
+  const [selectedPage, setSelectedPage] = useState(1);
+
+  const handlePageChange = (event, page) => {
+    setSelectedPage(page);
+  };
+
+  const page = {
+    pageSize:10,
+    pageNumber:selectedPage
+  }
+
+  const totalPage = useAxiosForSayisalLotoPagination(page);
+
+  const {data, isError, isFething} = useFetchSayisalLotoQuery(page);
+  if(isFething || data === undefined) {
+    return (
+      <Box style={{ width: '100%', height: '100vh' }}>
+        <CircularProgress className='spinnerAdminSayisalLoto' />
+      </Box>
+    )
+  }
+
+  console.log(data);
+
   return (
-    <div>AdminSayisalLoto</div>
+    <div>AdminSayisa lLoto</div>
   )
 }
 
